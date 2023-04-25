b7b69dfb336ded64af8d3e0513394714ca73170c
Sanjay <sanjay.e@strat-agile.com>
2023-04-25 10:20:47 +0530

    card

--- complete-react-cource/src/App.js
+++ complete-react-cource/src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
+import Card from './components/Card';
 import './components/ExpenceItem';
 import ExpenceItem from './components/ExpenceItem';
 
 function App() {
   const Expence = [
     { id: 'e1', date: new Date(2021, 5, 12), title: 'pen', rate: '100$' },
-    { id: 'e2', date: 'May 09 2023', title: 'hen', rate: '200$' },
-    { id: 'e3', date: 'jun 09 2023', title: 'ben', rate: '300$' },
-    { id: 'e4', date: 'dec 09 2023', title: 'ten', rate: '400$' }
+    { id: 'e2', date: new Date(2021, 5, 12), title: 'hen', rate: '200$' },
+    { id: 'e3', date: new Date(2021, 5, 12), title: 'ben', rate: '300$' },
+    { id: 'e4', date: new Date(2021, 5, 12), title: 'ten', rate: '400$' }
   ]
   return (
-    <div>
+    <Card>
       <ExpenceItem
         title={Expence[0].title}
         date={Expence[0].date}
@@ -31,7 +32,7 @@ function App() {
         date={Expence[3].date}
         rate={Expence[3].rate}
       ></ExpenceItem>
-    </div >
+    </Card >
   );
 }
 
--- complete-react-cource/src/components/ExpenceItem.js
+++ complete-react-cource/src/components/ExpenceItem.js
@@ -1,13 +1,15 @@
 import '../components/ExpenceItem.scss';
+import Card from './Card';
 import ExpenceDate from './ExpenceDate';
 
 function ExpenceItem(props) {
+    console.log(props);
     return (
-        <div className='expenceitem' >
+        <Card className='expenceitem' >
             <ExpenceDate date={props.date} />
             <p className='expenceitem__title'>{props.title}</p>
             <span className='expenceitem__rate'>{props.rate}</span>
-        </div>
+        </Card>
     )
 }
 
