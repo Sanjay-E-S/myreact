0c204e01da2d7920d597f0b84bdff6a11fd5687d
Sanjay <sanjay.e@strat-agile.com>
2023-05-02 18:36:27 +0530

    form submission

--- complete-react-cource/src/components/Form.js
+++ complete-react-cource/src/components/Form.js
@@ -2,52 +2,68 @@ import { useState } from 'react';
 import '../components/Form.scss';
 
 const Form = () => {
-    // const [enteredTitle, setTitle] = useState('');
-    // const [enteredDate, setDate] = useState('');
-    // const [enteredAmount, setAmount] = useState('');
+    const [enteredTitle, setTitle] = useState('');
+    const [enteredDate, setDate] = useState('');
+    const [enteredAmount, setAmount] = useState('');
 
-    const [userinput, setUserInput] = useState({
-        enteredTitle: '',
-        enteredDate: '',
-        enteredAmount: '',
-    });
+    // const [userinput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredDate: '',
+    //     enteredAmount: '',
+    // });
 
     const titleHandler = (e) => {
-        setUserInput({
-            ...userinput,
-            enteredTitle: e.target.value,
-        });
+        // setUserInput({
+        //     ...userinput,
+        //     enteredTitle: e.target.value,
+        // });
+        setTitle(e.target.value);
     }
     const dateHandler = (e) => {
-        setUserInput({
-            ...userinput,
-            enteredDate: e.target.value,
-        });
+        // setUserInput({
+        //     ...userinput,
+        //     enteredDate: e.target.value,
+        // });
+        setDate(e.target.value);
     }
     const amountHandler = (e) => {
-        setUserInput({
-            ...userinput,
-            enteredAmount: e.target.value,
-        });
+        // setUserInput({
+        //     ...userinput,
+        //     enteredAmount: e.target.value,
+        // });
+        setAmount(e.target.value);
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        const enteredData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        console.log(enteredData);
+        setAmount('');
+        setTitle('');
+        setDate('');
     }
 
     return (
         <div className='formouter' >
-            <form>
+            <form onSubmit={submitHandler} >
                 <div className='formitem'>
                     <label>Date</label>
-                    <input className="forminput" type="date" min={'2023-01-01'} max={'2023-12-31'} onChange={dateHandler}></input>
+                    <input className="forminput" value={enteredDate} type="date" min={'2023-01-01'} max={'2023-12-31'} onChange={dateHandler}></input>
                 </div>
                 <div className='formitem'>
                     <label>Title</label>
-                    <input className="forminput" type="text" onChange={titleHandler}></input>
+                    <input className="forminput" value={enteredTitle} type="text" onChange={titleHandler}></input>
                 </div>
                 <div className='formitem'>
                     <label>Amount</label>
-                    <input className="forminput" type="text" onChange={amountHandler}></input>
+                    <input className="forminput" value={enteredAmount} type="text" onChange={amountHandler}></input>
                 </div>
+                <button type='submit' className='submitbtn'>Submit</button>
             </form>
-            <button type='submit' className='submitbtn'>Submit</button>
         </div >
     )
 }
